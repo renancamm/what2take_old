f90e28eea05caf21464774b67ec8a4dca675b7ba
Renan Cammarosano <renancamm@gmail.com>
2018-04-30 00:35:50 +0100

    add category column on Product admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Category, Product, Travel
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+
+
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Travel)
