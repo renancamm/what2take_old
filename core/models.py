ccee3f56102c3f4bb49a1b366aa8e625c9580706
Renan Cammarosano <renancamm@gmail.com>
2018-04-30 00:21:43 +0100

    Add str method to Category and Product

--- core/models.py
+++ core/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Travel(models.Model):
