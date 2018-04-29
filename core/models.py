c5205e0e73f22fa551bb2f46038c7a7b7a044744
Renan Cammarosano <renancamm@gmail.com>
2018-04-30 00:06:45 +0100

    add Product model

--- core/models.py
+++ core/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
