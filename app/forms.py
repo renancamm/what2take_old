0de44bd9c87fbe9de9d8b48d48b62c32272467ba
Renan Cammarosano <renancamm@gmail.com>
2018-05-12 18:36:07 +0100

    update templates for cleaner UI

--- app/forms.py
+++ app/forms.py
@@ -4,9 +4,9 @@ from .models import Backpack
 
 class BackpackForm(forms.ModelForm):
 
-    temp = forms.ChoiceField(label='Temperatura', choices=Backpack.TEMP_CHOICES, widget=forms.RadioSelect())
-    days = forms.ChoiceField(label='Dias', choices=Backpack.DAYS_CHOICES, widget=forms.RadioSelect())
-    sex = forms.ChoiceField(label='Sexo', choices=Backpack.SEX_CHOICES, widget=forms.RadioSelect())
+    temp = forms.ChoiceField(label='Temperatura', choices=Backpack.TEMP_CHOICES, initial=20, widget=forms.RadioSelect())
+    days = forms.ChoiceField(label='Dias', choices=Backpack.DAYS_CHOICES, initial=7, widget=forms.RadioSelect())
+    sex = forms.ChoiceField(label='Sexo', choices=Backpack.SEX_CHOICES, initial='f', widget=forms.RadioSelect())
 
     class Meta:
         model = Backpack
